be95f0ee5623eb21ab0b16ffd6607fa41e6843d3
Ubuntu <mccarleyd@hotmail.com>
2019-10-18 21:05:25 +0000

    Test

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://best-for-you-db-dwm:MitSxRQXvWi0hLze4rzr14qTJxWllOFm4Vnr9ZHM1NgjLXKLLGJiitX3Q7SvfMfDauD08fL10nKDn55mW8FSaw==@best-for-you-db-dwm.documents.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
